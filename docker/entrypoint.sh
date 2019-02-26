ef6bc2a17bdade8e20f4bb66a3e808f21c7c0dd8
Joel Tobey <joel.tobey@domo.com>
2019-02-26 09:49:44 -0700

    bash script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [[ $DEBUG_MODE = 'true' ]]; then
+    echo "export GRADLE_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
     export GRADLE_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005
-    sh /app/gradlew :app:bootRun
-else
-    sh /app/gradlew :app:bootRun
 fi
+
+echo "Running :app:bootRun"
+bash /app/gradlew :app:bootRun
